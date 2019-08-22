d7cf93b95bc97e2200440297a57b7b98866f0b67
Diego Vega <diego.vega@microsoft.com>
2019-08-22 14:31:07 -0700

    Add project files.

---
+++ EdmxDotNetCoreApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Xml;
+
+namespace EdmxDotNetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new Entities())
+            {
+                db.Database.CreateIfNotExists();
+                Console.WriteLine(db.People.Include(p=>p.Things).Count());
+            }
+        }
+    }
+}
